ceb2b533fd6afc8d6f8ad42addb5877b0500192c
Douglas Costa Rodrigues <douglas@technext.com.br>
2023-05-05 18:49:57 -0300

    Injecao de dependencia

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -20,15 +20,7 @@ class FilaPrioritaria(FilaBase):
         return (f'\nCliente atual: {cliente_atual}, ') +  \
             (f'dirija-se ao caixa: {caixa}')
 
-    def estatistica(self, dia: str, agencia: int, flag: str)-> dict:
-        estatistica: Dict[str, Union[List[str], str, int]] = {}
-        if flag != 'detail':
-            estatistica[f'{agencia}-{dia}'] = len(self.clientes_atendidos)
-        else:
-            estatistica['dia'] = dia
-            estatistica['agencia'] = agencia
-            estatistica['clientes_atendidos'] = self.clientes_atendidos
-            estatistica['quantidade_clientes_atendidos'] = (
-                            len(self.clientes_atendidos))
+    def estatistica(self, dia: str, agencia: int, retorna_estatistica)-> dict:
+        estatistica = retorna_estatistica(dia, agencia)
 
-        return estatistica
+        return estatistica.roda_estatistica(self.clientes_atendidos)
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from fila_prioritaria import FilaPrioritaria
 #print(fila_teste.chamacliente(5))
 #print(fila_teste.chamacliente(10))
 from fabrica_fila import FabricaFila
-
+from estatistica_resumida import EstatisticaResumida
+from estatistica_detalhada import EstatisticaDetalhada
 
 #fila_teste2 = FilaNormal()
 #fila_teste2.atualiza_fila()
@@ -18,8 +19,11 @@ from fabrica_fila import FabricaFila
 #print(fila_teste2.chama_cliente(1))
 #print(fila_teste2.estatistica('10/01/1994', 197, 'detail'))
 
-teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica = FabricaFila.pega_fila('prioritaria')
+teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.estatistica('20/03/2025', 120, EstatisticaResumida))
+print(teste_fabrica.estatistica('20/03/2025', 120, EstatisticaDetalhada))
